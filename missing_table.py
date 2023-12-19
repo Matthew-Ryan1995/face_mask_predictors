aba21fd34e53da19ad4eeff22ad10915bfaf61c6
Jinjingggg <2422249798@qq.com>
2023-12-19 13:42:21 +1030

    updated plots

--- missing_table.py
+++ missing_table.py
@@ -24,5 +24,20 @@ df = pd.read_csv("raw_data/australia_a.csv")
 
 
 # Double Check NULL value in these variables
+missing_value_counts = {}
 for col in df:
-    print(f"{col : >16} has total amount of missing value of {df[col].isnull().sum()}")
+    # Count missing values in each column
+    missing_count = df[col].isnull().sum()
+    
+    # Store the information in the dictionary
+    missing_value_counts[col] = missing_count
+
+    # print(f"{col : >16} has total amount of missing value of {df[col].isnull().sum()}")
+        
+
+# Convert the dictionary to a DataFrame and sort it by missing value count
+missing_value_df = pd.DataFrame(list(missing_value_counts.items()), columns=['Variable Name', 'Missing Value Count'])
+missing_value_df = missing_value_df.sort_values(by='Missing Value Count')
+
+# Save the DataFrame to a CSV file
+missing_value_df.to_csv('missing_value_counts.csv', index=False)
--- reproduce_UK.py
+++ reproduce_UK.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import seaborn as sns
-import matplotlib.pyplot as plt
 from datetime import datetime
+import matplotlib.pyplot as plt
 
 # Function to convert the date format
 def convert_date_format(date_str):
@@ -18,7 +18,6 @@ def convert_date_format(date_str):
     except ValueError:
         raise ValueError(f"Unsupported date format: {date_str}")
 
-# Read the CSV file
 df = pd.read_csv("raw_data/united-kingdom.csv", encoding='ISO-8859-1')  
 
 # Frequency dictionary
@@ -29,11 +28,10 @@ for col in df.columns:
     if col.startswith("i12_health_"):
         df[col] = df[col].map(frequency_dict)
 
-# Convert "endtime" to datetime format
+# convert "endtime" with date %m/%d/%Y
 df["endtime"] = df["endtime"].apply(convert_date_format)
 df["endtime"] = pd.to_datetime(df["endtime"])   
 df = df.sort_values(by=["endtime"])
-
 # Calculate the median for the behavior scale for each unique date
 df["behavior_scale"] = df[["i12_health_1", "i12_health_2", "i12_health_3", "i12_health_4", "i12_health_5", "i12_health_6", "i12_health_7", "i12_health_8", "i12_health_9", "i12_health_10", "i12_health_11", "i12_health_12", "i12_health_13", "i12_health_14", "i12_health_15", "i12_health_16", "i12_health_17", "i12_health_18", "i12_health_19", "i12_health_20", "i12_health_21", "i12_health_22", "i12_health_23", "i12_health_24", "i12_health_25", "i12_health_26", "i12_health_27", "i12_health_28","i12_health_29"]].mean(axis=1)
 
@@ -45,28 +43,21 @@ valid_endtimes = df_grouped_size[df_grouped_size >= 500].index
 
 # Filter the original DataFrame to keep only rows with valid endtimes
 df_filtered = df[df["endtime"].isin(valid_endtimes)]
-
 # Calculate the median for the filtered DataFrame
 df_grouped = df_filtered.groupby("endtime")["behavior_scale"].median()
 
-# Set specific dates for the x-axis ticks
-specific_dates = ["15/06/2020", "25/01/2021", "19/07/2021", "10/01/2022", "20/06/2022"]
+# Create a box plot using seaborn
+df_filtered.index = df_filtered.index.astype(str)
+df_grouped.index = df_grouped.index.astype(str)
 
-# Visualization using seaborn for the line plot
+sns.set()
+
 plt.figure(figsize=(12, 6))
-sns.lineplot(x=df_grouped.index, y=df_grouped.values, marker='o', linestyle='-')
-sns.boxplot(x=df_filtered["endtime"], y=df_filtered["behavior_scale"])
-
-# Customize the plot
-plt.title('Median Behavioral Dynamics over Time in UK')
+ax = sns.boxplot(x=df_filtered["endtime"], y=df_filtered["behavior_scale"])
+ax = sns.lineplot(x=df_grouped.index, y=df_grouped.values, linestyle='-', linewidth = 1.5, color='g')
+plt.xticks(rotation=45, ha="right")  # Rotate x-axis labels for better readability
+plt.title('Behavioral Dynamics over Time in UK')
 plt.xlabel('Time')
-plt.ylabel('Median Behavior Scale')
-plt.ylim(0, 5)
-# plt.xticks([convert_date_format(date) for date in specific_dates])
-
-# Rotate x-axis labels for better readability
-plt.xticks(rotation=45)
-
-# Show the plot
+plt.ylabel('Behavior Scale')
 plt.tight_layout()
 plt.show()
