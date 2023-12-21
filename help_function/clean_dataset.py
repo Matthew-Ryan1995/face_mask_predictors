150b98b113433b147b79db72a8eee27e0734ab50
Jinjingggg <2422249798@qq.com>
2023-12-21 14:28:47 +1100

    integrate the new_week_col.py into cleaned_data.py

--- help_function/clean_dataset.py
+++ help_function/clean_dataset.py
@@ -15,13 +15,28 @@ Date created:
 
 # filter out those with missing values larger than 5000
 import pandas as pd
-import datetime as datetime
 from missing_table import missing_value_df
+from datetime import datetime
+
+# This function converts the string into the type of datetime %d/%m/%Y
+def convert_date_format(date_str):
+    try:
+        date_time_obj = datetime.strptime(date_str, '%d/%m/%Y %H:%M')
+        formatted_date = date_time_obj.strftime('%m/%d/%Y')
+        return formatted_date
+    except ValueError:
+        pass
+
+    try:
+        date_time_obj = datetime.strptime(date_str, '%d/%m/%Y')
+        return date_str
+    except ValueError:
+        raise ValueError(f"Unsupported date format: {date_str}")
 
 # note: More needs to be added to this script to clean the columns as needs be
 
 # edit: changed filed path
-df = pd.read_csv("../raw_data/australia.csv")
+df = pd.read_csv("raw_data/australia.csv")
 # Set the threshold for missing value count
 # edit: change cound to 1006
 thresh_value = 1006
@@ -29,15 +44,21 @@ thresh_value = 1006
 # Extract the variable names with missing value counts larger than 1000
 columns_to_drop = missing_value_df.loc[missing_value_df['Missing Value Count']
                                        > thresh_value, 'Variable Name'].tolist()
-columns_to_drop.append('q_other')  # __NA__ covers almost all of the columns
-# __NA__ covers almost all of the columns
-columns_to_drop.append('i14_health_other')
-
 
 df.drop(columns=columns_to_drop, inplace=True)
 df.dropna(inplace=True)
-# print(df)
+
+# create a new column in the csv that computer from week 1 for every two weeks 
+df["endtime"] = df["endtime"].apply(convert_date_format)
+df["endtime"] = pd.to_datetime(df["endtime"])
+
+# Find the start date (minimum date) and end date (maximum date)
+start_date = df['endtime'].min()
+end_date = df['endtime'].max()
+
+# Create a new column 'week_number' and assign week numbers
+df['week_number'] = ((df['endtime'] - start_date).dt.days // 14) + 1
 
 # Save the cleaned DataFrame to a new CSV file
 # edit: fixed file path/save location
-df.to_csv("../data/cleaned_data.csv", index=False)
+df.to_csv("data/cleaned_data.csv", index=False)
--- help_function/missing_table.py
+++ help_function/missing_table.py
@@ -1,25 +1,28 @@
+'''
+This script will read in the dataset, find the missing values for each variable, and output into a csv file named "data/missing_value_counts.csv".
+    
+Author:
+    Jinjing Ye
+    
+Date created:
+    23/12/2023
+'''
+
 import pandas as pd
-import matplotlib.pyplot as plt
-import csv
-first_line = True
+    
 # edit added ../ so code would run
-df = pd.read_csv("../raw_data/australia_a.csv",
+df = pd.read_csv("raw_data/australia.csv",
                  na_values=[" ", "__NA__"])  # edit: This makes pandas read these values in the csv as missing
 
 # Double Check NULL value in these variables
 missing_value_counts = {}
 for col in df:
-    # Count missing values in each column
-    # missing_count = (df[col] == " ").sum()
 
     # edit: We can now just count which columns have missing values
     missing_count = (df[col].isna()).sum()
 
     # Store the information in the dictionary
     missing_value_counts[col] = missing_count
-
-    # print(f"{col : >16} has total amount of missing value of {df[col].isnull().sum()}")
-
 
 # Convert the dictionary to a DataFrame and sort it by missing value count
 missing_value_df = pd.DataFrame(list(missing_value_counts.items()), columns=[
@@ -29,4 +32,4 @@ missing_value_df = missing_value_df.sort_values(
 
 # Save the DataFrame to a CSV file
 # edit: File path changed so things save properly
-missing_value_df.to_csv('../data/missing_value_counts.csv', index=False)
+missing_value_df.to_csv('data/missing_value_counts.csv', index=False)
