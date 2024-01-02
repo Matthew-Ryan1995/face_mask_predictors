998dba1c7fb71b015f6852005bef012816612ae8
Jinjingggg <2422249798@qq.com>
2024-01-03 09:23:57 +1030

    reproduce is in endtime

--- code/reproduce_fig.py
+++ code/reproduce_fig.py
@@ -18,7 +18,7 @@ def convert_date_format(date_str):
     except ValueError:
         raise ValueError(f"Unsupported date format: {date_str}")
 
-df = pd.read_csv("raw_data/united-kingdom.csv", encoding='ISO-8859-1')  
+df = pd.read_csv("raw_data/australia.csv", encoding='ISO-8859-1')  
 
 # Frequency dictionary
 frequency_dict = {"Always": 5, "Frequently": 4, "Sometimes": 3, "Rarely": 2, "Not at all": 1}
@@ -35,11 +35,11 @@ df = df.sort_values(by=["endtime"])
 # Calculate the median for the behavior scale for each unique date
 df["behavior_scale"] = df[["i12_health_1", "i12_health_2", "i12_health_3", "i12_health_4", "i12_health_5", "i12_health_6", "i12_health_7", "i12_health_8", "i12_health_9", "i12_health_10", "i12_health_11", "i12_health_12", "i12_health_13", "i12_health_14", "i12_health_15", "i12_health_16", "i12_health_17", "i12_health_18", "i12_health_19", "i12_health_20", "i12_health_21", "i12_health_22", "i12_health_23", "i12_health_24", "i12_health_25", "i12_health_26", "i12_health_27", "i12_health_28","i12_health_29"]].mean(axis=1)
 
-# Filter out the day with less than 500 data points
+# Filter out the day with less than 500/300 data points
 df_grouped_size = df.groupby("endtime").size()
 
-# Filter out groups with sizes less than 500
-valid_endtimes = df_grouped_size[df_grouped_size >= 500].index
+# Filter out groups with sizes less than 500/300
+valid_endtimes = df_grouped_size[df_grouped_size >= 300].index
 
 # Filter the original DataFrame to keep only rows with valid endtimes
 df_filtered = df[df["endtime"].isin(valid_endtimes)]
@@ -56,8 +56,11 @@ plt.figure(figsize=(12, 6))
 ax = sns.boxplot(x=df_filtered["endtime"], y=df_filtered["behavior_scale"])
 ax = sns.lineplot(x=df_grouped.index, y=df_grouped.values, linestyle='-', linewidth = 1.5, color='g')
 plt.xticks(rotation=45, ha="right")  # Rotate x-axis labels for better readability
-plt.title('Behavioral Dynamics over Time in UK')
+# plt.title('Behavioral Dynamics over Time in UK')
+plt.title('Behavioral Dynamics over Time in AU')
 plt.xlabel('Time')
 plt.ylabel('Behavior Scale')
 plt.tight_layout()
 plt.show()
+
+# change this plot to proportion of male and female
