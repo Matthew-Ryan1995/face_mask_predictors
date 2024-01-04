2218c902d1bc55d6a70d34fd52a734971062424b
Matt Ryan <rya200@csiro.au>
2024-01-04 14:29:56 +1030

    Update to cleaning script

--- help_function/clean_dataset.py
+++ help_function/clean_dataset.py
@@ -19,6 +19,8 @@ from missing_table import missing_value_df
 from datetime import datetime
 
 # This function converts the string into the type of datetime %d/%m/%Y
+
+
 def convert_date_format(date_str):
     try:
         date_time_obj = datetime.strptime(date_str, '%d/%m/%Y %H:%M')
@@ -35,21 +37,23 @@ def convert_date_format(date_str):
 
 # note: More needs to be added to this script to clean the columns as needs be
 
+
 # edit: changed filed path
 df = pd.read_csv("../raw_data/australia.csv",
-                 na_values=[" ", "__NA__"], keep_default_na = True)
+                 na_values=[" ", "__NA__"], keep_default_na=True)
 # Set the threshold for missing value count
-# edit: change cound to 1006
-thresh_value = 1006
+# edit: change cound to 10781
+thresh_value = 10781
 
 # Extract the variable names with missing value counts larger than 1000
 columns_to_drop = missing_value_df.loc[missing_value_df['Missing Value Count']
                                        > thresh_value, 'Variable Name'].tolist()
 
 df.drop(columns=columns_to_drop, inplace=True)
-df.dropna(inplace=True)
 
-# create a new column in the csv that computer from week 1 for every two weeks 
+# Add N/A category to d1 and PHQ columns for specific weeks (from 19/02/21 to 18/10/21)
+
+# create a new column in the csv that computer from week 1 for every two weeks
 df["endtime"] = df["endtime"].apply(convert_date_format)
 df["endtime"] = pd.to_datetime(df["endtime"])
 
@@ -59,6 +63,13 @@ end_date = df['endtime'].max()
 
 # Create a new column 'week_number' and assign week numbers
 df['week_number'] = ((df['endtime'] - start_date).dt.days // 14) + 1
+
+
+# Add in cleaning of other variables as well
+
+# Do this last
+df.dropna(inplace=True)
+
 
 # Save the cleaned DataFrame to a new CSV file
 # edit: fixed file path/save location
