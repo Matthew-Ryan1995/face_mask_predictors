1adb786d3fdf9373ffcbf5e5f7b37d7cfd6751d7
Jinjingggg <2422249798@qq.com>
2024-01-05 15:48:36 +1030

    squash the d1_health into 1 column

--- help_function/clean_dataset.py
+++ help_function/clean_dataset.py
@@ -15,7 +15,7 @@ Date created:
 
 # filter out those with missing values larger than 5000
 import pandas as pd
-from missing_table import missing_value_df
+# from missing_table import missing_value_df # Change: directly import the output from the missing_table
 from datetime import datetime
 
 # This function converts the string into the type of datetime %d/%m/%Y
@@ -45,67 +45,45 @@ df = pd.read_csv("raw_data/australia.csv",
 # edit: change cound to 10781
 thresh_value = 10781
 
-# Extract the variable names with missing value counts larger than 1000
+# Extract the variable names with missing value counts larger than 10781
+missing_value_df = pd.read_csv('data/missing_value_counts.csv')
 columns_to_drop = missing_value_df.loc[missing_value_df['Missing Value Count']
                                        > thresh_value, 'Variable Name'].tolist()
 
 df.drop(columns=columns_to_drop, inplace=True)
 
-# Add N/A category to d1 and PHQ columns for specific weeks (from 19/02/21 week25 to 18/10/21 week43)
+# Add N/A category to d1 and PHQ columns for specific weeks (from 10/02/21 week25 to 18/10/21 week43)
 # Add n/a category to d1 and PHQ to clean the dataset to add in another category in weeks 
 df["endtime"] = df["endtime"].apply(convert_date_format)
 df["endtime"] = pd.to_datetime(df["endtime"])
-sdate = "19-02-2021"
+sdate = "10-02-2021" # Change: sdate -> 10-02-2021
 edate = "18-10-2021"
 mask = (df["endtime"] <= edate) & (df["endtime"] >= sdate)
+# Change: squash d1_health into one column
+df["d1_health_atleastone"] = "No"
+for index, row in df.iterrows():
+    for col in df.columns:
+        if col.startswith("d1_health") and col.endswith("1" or "2" or "3"or "4"or "5"or "6"or "7"or "8"or "9"or "10"or "11"or "12"or "13"):
+            if row[col] == "Yes":
+                df.at[index, "d1_health_atleastone"] = "Yes"
+                break  # Exit the loop once a value of 1 is found
+    
 
-# Clean the d1_health and PHQ4
-phq_answer = {"Not at all": 1, "Several days": 2, "More than half the days": 3, "Nearly every day": 4, "Prefer not to say": 5}
-for i in range(1,5):
-    # print(df.loc[mask, f"PHQ4_{i}"].isnull().sum())
-    df.loc[mask, f"PHQ4_{i}"] = df.loc[mask, f"PHQ4_{i}"].fillna("N/A")
-    df[f"PHQ4_{i}"] = df[f"PHQ4_{i}"].map(phq_answer)
-    # print(df.loc[mask, f"PHQ4_{i}"].isnull().sum())
+# Change: have a face mask variable to be target in model
+# Change: compare those wear mask with diagonsed and without diagonosed
 
-d1_answer = {"Yes": 1, "No": 0, "N/A": 99}
-for i in range(1,14):
-    # print(df.loc[mask, f"d1_health_{i}"].isnull().sum())
-    df.loc[mask, f"d1_health_{i}"] = df.loc[mask, f"d1_health_{i}"].fillna("N/A")
-    df[f"d1_health_{i}"] = df[f"d1_health_{i}"].map(d1_answer)
-    # print(df.loc[mask, f"d1_health_{i}"].isnull().sum())
-
-for i in range(98,100):
-    # print(df.loc[mask, f"d1_health_{i}"].isnull().sum())
-    df.loc[mask, f"d1_health_{i}"] = df.loc[mask, f"d1_health_{i}"].fillna("N/A")
-    df[f"d1_health_{i}"] = df[f"d1_health_{i}"].map(d1_answer)
-    # print(df.loc[mask, f"d1_health_{i}"].isnull().sum())
-
-wcr_answer = {"Very well":1, "Somewhat well": 2, "Somewhat badly": 3, "Very badly": 4, "Don't know": 5}
-for i in range(1,3):
-    df[f"WCRex{i}"] = df[f"WCRex{i}"].map(wcr_answer)
 # Add in cleaning of other variables as well    
 # Drop the useless columns
-df = df.drop(["RecordNo", "household_size", "qweek", "weight"], axis = 1)
+df = df.drop(["RecordNo", "qweek", "weight"], axis = 1)
 
 # Convert the string into values in scale
 for i in range(1, 3):
-    df[f"r1_{i}"] = df[f"r1_{i}"].replace({"7 - Agree": 7, "1 – Disagree": 1})
+    df[f"r1_{i}"] = df[f"r1_{i}"].replace({"7 - Agree": 7, "6": 6, "5": 5, "4":4, "3": 3, "2": 2, "1 – Disagree": 1}) # Change: change the string to int
 
 frequency_dict = {"Always": 5, "Frequently": 4, "Sometimes": 3, "Rarely": 2, "Not at all": 1}
 for column in df.columns:
     if column.startswith("i12_health_"):
         df[column] = df[column].map(frequency_dict)
-
-i9_answer = {"Yes": 1, "No": 0, "Not sure": 99}
-df["i9_health"] = df["i9_health"].map(i9_answer)
-
-i11_answer = {"Very willing": 1, "Somewhat willing": 2, "Neither willing nor unwilling": 3 ,"Somewhat unwilling": 4, "Very unwilling": 5, "Not sure": 6}
-df["i11_health"] = df["i11_health"].map(i11_answer)
-
-employ_answer = {"Full time employment": 1, "Part time employment": 2, "Full time student": 3, "Retired": 4, "Unemployed": 5, "Not working":6, "Other":7}
-df["employment_status"] = df["employment_status"].map(employ_answer)
-# Compare those columns with similar factor i.e. face mask
-
     
 # create a new column in the csv that computer from week 1 for every two weeks
 
@@ -121,7 +99,6 @@ df['week_number'] = ((df['endtime'] - start_date).dt.days // 14) + 1
 # Do this last
 df.dropna(inplace=True)
 
-print(df["r1_1"].value_counts().index.tolist())
 # Save the cleaned DataFrame to a new CSV file
 # edit: fixed file path/save location
 df.to_csv("data/cleaned_data.csv", index=False)
