acb906a435e877d8e5f776829d9c0dbe8e3eec3b
Jinjingggg <2422249798@qq.com>
2024-02-13 12:23:34 +1100

    logistic regression change back to 5 fold

--- code/basic_model/logistic_regression.py
+++ code/basic_model/logistic_regression.py
@@ -10,8 +10,8 @@ y = pd.read_csv("../data/y_train.csv", keep_default_na = False).values.ravel()
 
 # instantiate the model
 logreg = LogisticRegression(random_state=16, max_iter = 5000)
-kf = KFold(n_splits=10)
-cv_scores = cross_val_score(logreg, x, y, cv=5, scoring='accuracy')
+kf = KFold(n_splits= 5)
+cv_scores = cross_val_score(logreg, x, y, cv=kf, scoring='accuracy')
 
 # Print the accuracy scores for each fold
 print("Cross-validation scores:", cv_scores)
